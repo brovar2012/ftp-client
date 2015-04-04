894dd83ed005b293b75cc3d2edac84b10e4ce2ec
brovar2012 <brovar2012@gmail.com>
2015-04-05 01:19:28 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
-#include "mainwindow.h"
 #include <QApplication>
 #include "firstwindow.h"
+#include  "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     firstWindow *w = new firstWindow(0);
     w->show();
+    mainWindow *ww= new mainWindow(0);
+    ww->resize(850,400);
+    ww->show();
 
     return a.exec();
 }
