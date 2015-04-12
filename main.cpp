ecb99168d44eac52bfb73c552abaeae664faef2d
brovar2012 <brovar2012@gmail.com>
2015-04-12 16:00:25 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include <QApplication>
 #include "firstwindow.h"
 #include  "mainwindow.h"
+#include "qftp.h"
+#include "qurlinfo.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    firstWindow *w = new firstWindow(0);
-    w->show();
-    mainWindow *ww= new mainWindow(0);
-    ww->resize(850,400);
-    ww->show();
+    //firstWindow *w = new firstWindow(0);
+    //w->show();
+    mainWindow *window= new mainWindow(0);
+    window->resize(850,400);
+    window->show();
 
     return a.exec();
 }
