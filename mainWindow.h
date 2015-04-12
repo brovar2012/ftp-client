e5753db6ad6f0c42e49934f753b406ef16344e61
brovar2012 <brovar2012@gmail.com>
2015-04-12 16:02:23 +0300

    Update mainWindow.h

--- mainWindow.h
+++ mainWindow.h
@@ -11,23 +11,59 @@
 #include <QFileSystemModel>
 #include <QTreeView>
 #include <QMenuBar>
+#include <QTreeWidget>
+#include <QDialogButtonBox>
+#include <QProgressDialog>
+#include <QMessageBox>
+#include <QtNetwork>
+
+#include "qftp.h"
+#include "qurlinfo.h"
 
 class mainWindow:public QDialog
 {
     Q_OBJECT
 public:
     mainWindow(QWidget *parent=0);
-private:
+    QSize sizeHint();
+public:
+    // - для главного окна
     QMenuBar *menu;
-    QMenu *file;
+    QMenu *fileMenu;
     QMenu *help;
-    QLineEdit *ftp,*computer;
-    QFileSystemModel *modelOfFTP;
+    QLineEdit *ftpAdress,*computerAdress;
     QFileSystemModel *modelOfComp;
-    QTreeView *tableOfFTP;
     QTreeView *tableOfComp;
-private slots:
+    // - для окна FTP
+    QLabel *ftpServerLabel;
+    QLineEdit *ftpServerLineEdit;
+    QLabel *statusLabel;
+    QTreeWidget *fileList;
+    QPushButton *cdToParentButton;
+    QPushButton *connectButton;
+    QPushButton *downloadButton;
+    QPushButton *quitButton;
+    QDialogButtonBox *buttonBox;
+    QProgressDialog *progressDialog;
+    QHash<QString, bool> isDirectory;
+    QString currentPath;
+    QFtp *ftp;
+    QFile *file;
+public slots:
+    // для компа
     void see();
+    // для FTP
+    void connectOrDisconnect();
+    void downloadFile();
+    void cancelDownload();
+
+    void ftpCommandFinished(int commandId, bool error);
+    void addToList(const QUrlInfo &urlInfo);
+    void processItem(QTreeWidgetItem *item, int column);
+    void cdToParent();
+    void updateDataTransferProgress(qint64 readBytes,
+                                    qint64 totalBytes);
+    void enableDownloadButton();
 };
 
 #endif // MAINWINDOW_H
