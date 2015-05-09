ec9b01f3702ba71a9d6ca7cd2736a9dcad3e20f7
brovar2012 <brovar2012@gmail.com>
2015-05-09 19:26:42 +0300

    Update kurcash.cpp

--- kurcash.cpp
+++ kurcash.cpp
@@ -1,40 +1,28 @@
 #define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
-
 #ifndef _WINSOCKAPI_ 
 #include <winsock2.h>
 #endif
-
 #include <iostream>
 #include <string>
 #include <fstream>
 #include <windows.h>
-
-#define SLEEP 500
-
 #pragma comment(lib, "ws2_32.lib")
 
-char buff[65536];
+const int SLEEP = 500;
+const char *DIR_FOR_SAVING = "d:/";
 
 using namespace std;
 
-struct hostent *host, *host2;
-
-char addr[] = "127.0.0.1";
-
-std::fstream stream;
-
-unsigned int file_size;
-
-char file_name[256];
-
-SOCKET init_data(SOCKET s) {
+SOCKET init_data(SOCKET FTPsocket,char *addressOfServer) 
+{
 	SOCKET ds;
-	send(s, "PASV\r\n", strlen("PASV\r\n"), 0);
-	cout << endl << "C: PASV";
-	recv(s, buff, 1024, 0);
-	cout << endl << "S: " << buff; ////выводим на экран полученную от сервера строку
+	char buff[65536]= "";
+
+	send(FTPsocket, "PASV\r\n", strlen("PASV\r\n"), 0);
+	//cout << endl << "C: PASV";
+	recv(FTPsocket, buff, 1024, 0);
+	//cout << endl << "Server: " << buff; для получения контрольных цифр 
 
 	int a, b;
 	char *tmp_char;
@@ -45,32 +33,34 @@ SOCKET init_data(SOCKET s) {
 	sscanf(tmp_char, "%d,%d,%d,%d,%d,%d", &c, &d, &e, &f, &a, &b);
 	int len;
 	sockaddr_in address;
-	int result;
 
+	int result;
 	int port = a * 256 + b;
 
 	ds = socket(AF_INET, SOCK_STREAM, 0);
 	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = inet_addr(addr);   ///addr - у меня глобальная переменная с адресом сервера
+	address.sin_addr.s_addr = inet_addr(addressOfServer); 
 	address.sin_port = htons(port);
 
 	len = sizeof(address);
 	result = connect(ds, (sockaddr *)&address, len);
 	if (result == -1) {
-		perror("oops: client");
+		perror("Client error!");
 		return -1;
 	}
 
 	return ds;
 }
-void sending(char *a, SOCKET FTPsocket)
+
+void sending(char *str, SOCKET FTPsocket)
 {
-	send(FTPsocket, a, strlen(a), 0);
-	cout << "\nClient: " << a;
+	send(FTPsocket, str, strlen(str), 0);
+	cout << "\nClient: " << str;
 }
 
 void readServ(SOCKET FTPsocket)
 {
+	char buff[1024];
 	for (int i = 0; i<1024; i++)
 		buff[i] = '\0';
 	recv(FTPsocket, buff, 1024, 0);
@@ -78,31 +68,34 @@ void readServ(SOCKET FTPsocket)
 	Sleep(SLEEP);
 }
 
-int init_sock() {
+int init_sock (char *addressOfServer) 
+{
 
 	WSADATA WSAData;
 	if (WSAStartup(MAKEWORD(2, 0), &WSAData) != 0)
 	{
-		cout << "786";
-		// getch();
-		// exit(0);
+		cout << "Ошибка инициализации сокета." <<endl;
+		return -1;
 	}
 
-	SOCKET S;
+	SOCKET FTPSocket;
 	sockaddr_in address;
-	S = socket(AF_INET, SOCK_STREAM, 0);
+	struct hostent *host;
+
+	FTPSocket = socket(AF_INET, SOCK_STREAM, 0);
 	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = inet_addr(addr);// was 77.246.101.195    87.224.183.162    87.224.147.100    134.169.34.11 lin   
+	address.sin_addr.s_addr = inet_addr(addressOfServer);  
 	address.sin_port = htons(21);
+
 	if (address.sin_addr.s_addr == INADDR_NONE)
 	{
 		host = NULL;
-		host = gethostbyname(addr);
+		host = gethostbyname(addressOfServer);
 
 		if (host == NULL)
 		{
-			//            MessageBox::Show("SERVER CONNECTION ERROR");
-			exit(0);
+			cout << " Ошибка подключения к серверу." << endl;
+			return -1;
 		}
 		memcpy(&address.sin_addr, host->h_addr_list[0], host->h_length);
 	}
@@ -110,113 +103,27 @@ int init_sock() {
 	int len;
 	int result;
 	len = sizeof(address);
-	result = connect(S, (sockaddr *)&address, len);
+	result = connect(FTPSocket, (sockaddr *)&address, len);
 	if (result == -1)
 	{
-		cout << "87698";
-		//        getch();
-		exit(0);
+		cout << "Ошибка соединения." <<endl;
+		return -1;
 	}
-	cout << "Connection Estabilished!\n";
-	readServ(S);
-	return S;
 
+	cout << "Соединение установлено.\n";
+	readServ(FTPSocket);
+	return FTPSocket;
 }
 
-char *getdata(SOCKET ds)
+char *getdata(SOCKET FTPSocket)
 {
 	char file[65536];
 	for (int i = 0; i<65536; i++)
 		file[i] = '\0';
-	recv(ds, file, sizeof(file), 0);
-	closesocket(ds);
+	recv(FTPSocket, file, sizeof(file), 0);
+	closesocket(FTPSocket);
 	return file;
 }
-
-/*int sent(SOCKET FTPsocket2)
-{
-/*ifstream  file;
-file.open("d:/2.mp3",ios::binary);
-char singleBuffer[4096];
-while (!file.eof())
-{
-file >> singleBuffer[0];
-send(FTPsocket2, singleBuffer, sizeof(singleBuffer), 0);
-}
-
-file.close();*/
-//return 1;
-/*char *buffer;
-ifstream  file;
-file.open("d:/2.mp3", ios::binary);
-long n = sizeof(file);
-buffer = (char*)malloc(n * sizeof(char));
-
-//file.read(buffer, n);
-while (send(FTPsocket2, buffer, sizeof(buffer), 0))
-{
-file.read(buffer, n);
-
-}
-file.close();
-return 1;*/
-
-/*int getfile(SOCKET ds)
-{
-char *tmp_char;
-tmp_char = strtok(buff, "\r\n");
-char t[256];
-memset(t, '\0', sizeof(t));
-strcpy(t, tmp_char);
-int i = 44, u = 0;
-char n[256];
-memset(n, '\0', sizeof(n));
-while (t[i] != '\0'){
-n[u] = t[i];
-i++; u++;
-}
-u -= 8; i = 0;
-char e[256];
-memset(e, '\0', sizeof(e));
-while (n[u] != '('){
-e[i] = n[u];
-i++; u--;
-}
-reverse(e);
-file_size = atoi(e);
-int n_len = u - 1;
-for (int i = 0; i<n_len; i++)
-file_name[i] = n[i];
-
-stream.open(file_name, ios::out | ios::binary | ios::trunc);
-if (!stream){
-cout << "Error open\n";
-//        getch();
-return 0;
-}
-
-char file[4096];
-int num = 0;
-unsigned int k = 0;
-int размер_файла = 0;
-while (num = recv(ds, file, 4096, 0)){
-if (num<0){
-cout << "Error";
-printf(" %d\n", WSAGetLastError());
-break;
-}
-
-// записывается сразу в файл
-stream.write(file, num);
-размер_файла += num;
-cout << "Recieved " << num << " bytes\n";
-Sleep(5);
-}
-
-std::cout << "\nRecieved байт" << размер_файла;
-closesocket(ds);
-return 1;
-}*/
 
 void authorization(SOCKET FTPsocket)
 {
@@ -258,76 +165,56 @@ void authorization(SOCKET FTPsocket)
 	}
 }
 
-void getList(SOCKET FTPsocket, SOCKET DATA)
+void getList(SOCKET FTPsocket, SOCKET DATA, char *adressOfServer)
 {
-	DATA = init_data(FTPsocket);
+	DATA = init_data(FTPsocket,adressOfServer);
 	sending("LIST\r\n", FTPsocket);
-	Sleep(444);
-	readServ(FTPsocket);//Ответ
+	Sleep(SLEEP);
+	readServ(FTPsocket);
 
 	char file[65536];
-	strcpy(file, getdata(DATA));//Список
-	cout << file << endl;//Ответ -список
-	Sleep(444);
+	strcpy(file, getdata(DATA));
+	cout << file << endl;
+	Sleep(SLEEP);
 }
 
-int getfile1(SOCKET FTPsocket2,char *s)
-{
-	fstream file;
-	file.open(s, ios::out | ios::binary);
-
-	char buffer[256];
-	while (recv(FTPsocket2, buffer, sizeof(buffer), 0))
-		file << buffer;
-
-	file.close();
-	return 1;
-}
-
-//////////////////////
-/*int getfile1(SOCKET FTPsocket2)
-{
-	fstream file;
-	file.open("d:/1.mp3", ios::out | ios::binary);
-
-	char buffer[256];
-
-	while (recv(FTPsocket2, buffer, sizeof(buffer), 0))
-		file << buffer;
-
-	file.close();
-	return 1;
-}*/
-
-void getFile(SOCKET FTPsocket, SOCKET DATA)
+void getFile(SOCKET FTPsocket, SOCKET DATA,char *adressOfServer)
 {
 	// !!! sending("NLST\r\n", FTPsocket);
 
 	char inquiry[1000];
 	char fileName[400];
 	char outputFile[1010];
-	fstream file;
 
 	cout << "Введите имя файла (целиком) для скачивания" << endl;
 	cin >> fileName;
 
-	DATA = init_data(FTPsocket);
+	DATA = init_data(FTPsocket,adressOfServer);
 
 	sprintf(inquiry, "RETR %s\r\n", fileName);
 
 	sending(inquiry, FTPsocket);
 	readServ(FTPsocket);
-	Sleep(444);
+	Sleep(SLEEP);
 
-	sprintf(outputFile, "d:/%s", fileName);
+	sprintf(outputFile, "%s%s",DIR_FOR_SAVING ,fileName);
+	cout <<outputFile <<endl;
 
-	file.open(outputFile, ios::out | ios::binary);
+	FILE *file;
+	file = fopen(outputFile, "wb");  
+	int read = 0;
+	int fullSize;
 
-	char buffer[64];
-	while (recv(DATA, buffer, sizeof(buffer), 0))
-		file << buffer;
+	do 
+	{
+		char buff[2048];   
+		int readed = recv(DATA,buff,sizeof(buff),0);   
+		fwrite(buff,1,readed,file);  
+		fullSize = read;
+		read += readed;  
+    } while (fullSize != read);
 
-	file.close();
+	fclose(file);
 }
 
 void deleteFile(SOCKET FTPsocket)
@@ -340,28 +227,61 @@ void deleteFile(SOCKET FTPsocket)
 
 	sprintf(inquiry, "DELE %s\r\n", fileForDelete);
 	sending(inquiry, FTPsocket);
-	Sleep(444);
+	Sleep(SLEEP);
 	readServ(FTPsocket);
 }
 
-void goToDirectory(SOCKET FTPsocket, SOCKET DATA, char *nameOfDirectory)
+void goToDirectory(SOCKET FTPsocket, SOCKET DATA, char *nameOfDirectory,char *adressOfServer)
 {
 	char inquiry[1000];
 
 	sprintf(inquiry, "CWD %s\r\n", nameOfDirectory);
 	sending(inquiry, FTPsocket);
-	Sleep(444);
+	Sleep(SLEEP);
 	readServ(FTPsocket);
-	getList(FTPsocket, DATA);
+	getList(FTPsocket, DATA,adressOfServer);
 }
 
-void returnToParentsDir(SOCKET FTPsocket, SOCKET DATA)
+void returnToParentsDir(SOCKET FTPsocket, SOCKET DATA,char *addressOfServer)
 {
 	sending("XCUP\r\n", FTPsocket);
-	Sleep(444);
+	Sleep(SLEEP);
 	readServ(FTPsocket);
-	getList(FTPsocket, DATA);
+	getList(FTPsocket, DATA,addressOfServer);
 }
+
+void sendFile(SOCKET FTPsocket, SOCKET DATA,char *addressOfServer)
+{
+		/*ifstream  file;
+		file.open("d:/2.mp3",ios::binary);
+		char singleBuffer[4096];
+		while (!file.eof())
+		{
+			file >> singleBuffer[0];
+			send(FTPsocket, singleBuffer, sizeof(singleBuffer), 0);
+		}
+		file.close();
+		return 1;*/
+	DATA = init_data(FTPsocket,addressOfServer);
+		sending("STOR 33.mp3\r\n", FTPsocket);
+		readServ(FTPsocket);
+		system("pause");
+
+		FILE *file;
+		file = fopen("d:/33.mp3", "rb");  
+		int read = 0;
+		int fullSize=0;
+		do 
+		{
+			char buff[2048];   
+			int readed = send(DATA,buff,sizeof(buff),0);   
+			fread(buff,1,readed,file);  
+			fullSize++;
+			read += readed;  
+		} while (fullSize<1711);
+
+		fclose(file);
+} 
 
 int main()
 {
@@ -370,42 +290,96 @@ int main()
 	SOCKET DATA;
 	SOCKET FTPsocket;
 
-	cout << "Введите адрес FTP-сервера";
+
+	cout << "Введите адрес FTP-сервера" <<endl;//195.178.216.8   86.57.151.3    127.0.0.1
 	cin >> addressOfServer;
 
-	FTPsocket = init_sock();
-	char str[512];
+	FTPsocket = init_sock(addressOfServer);
 
 	authorization(FTPsocket);
 
-	DATA = init_data(FTPsocket);
+	DATA = init_data(FTPsocket,addressOfServer);
 
-	getList(FTPsocket, DATA);
+	getList(FTPsocket, DATA,addressOfServer);
 	system("pause");
 
-	//getFile(FTPsocket, DATA);
-	//system("pause");
+
+	sendFile (FTPsocket, DATA,addressOfServer);
+	system("pause");
+
+	//getFile(FTPsocket, DATA,addressOfServer);
+    //system("pause");
 
 	//deleteFile(FTPsocket);
 	//system("pause");
 
-	goToDirectory(FTPsocket, DATA, "123456");
-	system("pause");
+	//goToDirectory(FTPsocket, DATA, "123456");
+	//system("pause");
 
-	goToDirectory(FTPsocket, DATA, "222");
-	system("pause");
+	//goToDirectory(FTPsocket, DATA, "222");
+	//system("pause");
 
-	returnToParentsDir(FTPsocket, DATA);
-	system("pause");
+	//returnToParentsDir(FTPsocket, DATA);
+	//system("pause");
 
-	closesocket(FTPsocket);
-	WSACleanup();
-
-	stream.close();
-	sending("QUIT\r\n", FTPsocket);
+	//strcpy(pass, "/");
+	/*sending("LIST\r\n", S);
 	Sleep(444);
+	readServ(S);//Ответ
+	system("pause");
+
+	char file[65536];
+	strcpy(file, getdata(DATA));//Список
+	cout << file << endl;//Ответ -список
+
+	//sending("TYPE I\r\n", S);
+	//readServ(S);
+	//system("pause");
+	//sending("CWD ! ! Новинки КИНО\r\n", S);
+	//readServ(S);
+	//sending("CWD Шеф (комедия) [2012]\r\n", S);
+	//readServ(S);
+	Sleep(444);
+
+	//sending("STOR Mumiy_trol-03_Utekay.mp3\r\n", S);
+	//readServ(S);
+	//system("pause");
+
+	DATA = init_data(S);
+	sending("LIST\r\n", S);
+	Sleep(444);
+	readServ(S);
+	strcpy(file, getdata(DATA));//Список
+	cout << file << endl;//Ответ -список
+	system("pause");
+	//-------------------------------------------------Файл!!!*/
+
+	/*int k;
+	DATA = init_data(S);
+	sending("STOR 2.mp3\r\n", S);
+	readServ(S);
+	Sleep(444);
+	k = sent(DATA);*/
+	/*sending("DELE 5.mp3\r\n", S);
+	Sleep(444);
+	readServ(S);
+
+	sending("CWD 123456\r\n", S);
+	Sleep(444);
+	readServ(S);
+
+	DATA = init_data(S);
+	sending("LIST\r\n", S);
+	Sleep(444);
+	readServ(S);
+	strcpy(file, getdata(DATA));//Список
+	cout << file << endl;//Ответ -список*/
+
+	sending("QUIT\r\n", FTPsocket);
 	readServ(FTPsocket);
 	system("pause");
-	//    getch();
+
+	closesocket(FTPsocket);  ///закрытие соединения
+	WSACleanup();
 	return 0;
 }
