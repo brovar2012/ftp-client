16de82ad46605a1cc0e7d6f55511cf67b4096d5d
brovar2012 <brovar2012@gmail.com>
2015-05-14 21:17:07 +0300

    Update myFTP.cpp

--- myFTP.cpp
+++ myFTP.cpp
@@ -2,7 +2,7 @@
 
 int FTP::authorization()
 {
-	char str[512];
+	char str[MAX_STRING_LENGTH];
 	char choice;
 
 	cout << "Хотите авторизоваться?" << endl;
@@ -12,8 +12,8 @@ int FTP::authorization()
 
 	if (choice == '2')
 	{
-		char login[64];
-		char pass[64];
+		char login[MAX_STRING_LENGTH];
+		char pass[MAX_STRING_LENGTH];
 		cout << "Введите логин:" << endl;
 		cin >> login;
 		sprintf(str, "USER %s\r\n", login);
@@ -25,10 +25,10 @@ int FTP::authorization()
 		sprintf(str, "PASS %s\r\n", pass);
 		sending(str);
 
-		char buff[1024];
-		for (int i = 0; i<1024; i++)
+		char buff[MAX_BUFFER_LENGTH];
+		for (int i = 0; i<MAX_BUFFER_LENGTH; i++)
 			buff[i] = '\0';
-		recv(FTPsocket, buff, 1024, 0);
+		recv(FTPsocket, buff, MAX_BUFFER_LENGTH, 0);
 		if (buff[0] == '5')
 			return -1;
 		else
@@ -40,12 +40,12 @@ int FTP::authorization()
 	}
 	else
 	{
-		char login[64] = "anonymous";
+		char login[MAX_STRING_LENGTH] = "anonymous";
 		sprintf(str, "USER %s\r\n", login);
 		sending(str);
 		readServ();
 
-		char pass[64] = "anonymous@gmail.com";
+		char pass[MAX_STRING_LENGTH] = "anonymous@gmail.com";
 		sprintf(str, "PASS %s\r\n", pass);
 		sending(str);
 		readServ();
@@ -53,50 +53,50 @@ int FTP::authorization()
 	}
 }
 
-int FTP::initData() 
-	{
-		SOCKET ds;
-		char buff[65536]= "";
+int FTP::initData()
+{
+	SOCKET ds;
+	char buff[MAX_BUFFER_LENGTH] = "";
 
-		send(FTPsocket, "PASV\r\n", strlen("PASV\r\n"), 0);
-		//cout << endl << "C: PASV";
-		recv(FTPsocket, buff, 1024, 0);
-		//cout << endl << "Server: " << buff; для получения контрольных цифр 
+	send(FTPsocket, "PASV\r\n", strlen("PASV\r\n"), 0);
+	//cout << endl << "Client: PASV";
+	recv(FTPsocket, buff, MAX_BUFFER_LENGTH, 0);
+	//cout << endl << "Server: " << buff; //для получения контрольных цифр 
 
-		int a, b;
-		char *tmp_char;
-		tmp_char = strtok(buff, "(");
-		tmp_char = strtok(NULL, "(");
-		tmp_char = strtok(tmp_char, ")");
-		int c, d, e, f;
-		sscanf(tmp_char, "%d,%d,%d,%d,%d,%d", &c, &d, &e, &f, &a, &b);
-		int len;
-		sockaddr_in address;
+	int a, b;
+	char *tmp_char;
+	tmp_char = strtok(buff, "(");
+	tmp_char = strtok(NULL, "(");
+	tmp_char = strtok(tmp_char, ")");
+	int c, d, e, f;
+	sscanf(tmp_char, "%d,%d,%d,%d,%d,%d", &c, &d, &e, &f, &a, &b);
+	int len;
+	sockaddr_in address;
 
-		int result;
-		int port = a * 256 + b;
+	int result;
+	int port = a * 256 + b;
 
-		ds = socket(AF_INET, SOCK_STREAM, 0);
-		address.sin_family = AF_INET;
-		address.sin_addr.s_addr = inet_addr(addressOfServer); 
-		address.sin_port = htons(port);
+	ds = socket(AF_INET, SOCK_STREAM, 0);
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = inet_addr(addressOfServer);
+	address.sin_port = htons(port);
 
-		len = sizeof(address);
-		result = connect(ds, (sockaddr *)&address, len);
-		if (result == -1) 
-			return -1;
+	len = sizeof(address);
+	result = connect(ds, (sockaddr *)&address, len);
+	if (result == -1)
+		return -1;
 
-		DATA = ds;
-		return 0;
-	}
+	DATA = ds;
+	return 0;
+}
 
-int FTP::initSock () 
+int FTP::initSock()
 {
 
 	WSADATA WSAData;
 	if (WSAStartup(MAKEWORD(2, 0), &WSAData) != 0)
 	{
-		cout << "Ошибка инициализации сокета." <<endl;
+		cout << "Ошибка инициализации сокета." << endl;
 		return -1;
 	}
 
@@ -106,7 +106,7 @@ int FTP::initSock ()
 
 	FTPSocket = socket(AF_INET, SOCK_STREAM, 0);
 	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = inet_addr(addressOfServer);  
+	address.sin_addr.s_addr = inet_addr(addressOfServer);
 	address.sin_port = htons(21);
 
 	if (address.sin_addr.s_addr == INADDR_NONE)
@@ -137,75 +137,85 @@ int FTP::initSock ()
 
 char* FTP::getdata()
 {
-	char file[65536];
-	for (int i = 0; i<65536; i++)
+	char file[MAX_LIST_LENGTH];
+	for (int i = 0; i<MAX_LIST_LENGTH; i++)
 		file[i] = '\0';
 	recv(DATA, file, sizeof(file), 0);
 	closesocket(DATA);
 	return file;
 }
 
-void FTP::getList()
+void FTP::getList(char *list)
 {
 	initData();
 	sending("LIST\r\n");
 	Sleep(SLEEP);
 	readServ();
-
-	char file[65536];
-	strcpy(file, getdata());
-	cout << file << endl;
+	strcpy(list, getdata());
 	Sleep(SLEEP);
 }
 
-void FTP::getFile(char *fileName)
+int FTP::getFile(char *fileName)
 {
-	// !!! sending("NLST\r\n", FTPsocket);
-
-	char inquiry[500];
-	char outputFile[510];
+	char inquiry[MAX_STRING_LENGTH];
+	char outputFile[MAX_STRING_LENGTH];
 
 	initData();
 
 	sprintf(inquiry, "RETR %s\r\n", fileName);
 
 	sending(inquiry);
-	readServ();
-	Sleep(SLEEP);
+	char buff[MAX_BUFFER_LENGTH];
+	for (int i = 0; i<MAX_BUFFER_LENGTH; i++)
+		buff[i] = '\0';
+	recv(FTPsocket, buff, MAX_BUFFER_LENGTH, 0);
 
-	sprintf(outputFile, "%s%s",DIR_FOR_SAVING ,fileName);
-
-	FILE *file;
-	file = fopen(outputFile, "wb");  
-	int read = 0;
-	int fullSize;
-
-	do 
+	if (buff[0] == '5')
 	{
-		char buff[2048];   
-		int readed = recv(DATA,buff,sizeof(buff),0);   
-		fwrite(buff,1,readed,file);  
-		fullSize = read;
-		read += readed;
-    } while (fullSize != read);
+		cout << buff << endl;
+		return -1;
+	}
 
-	readServ();
-	fclose(file);
+	else
+	{
+		sprintf(outputFile, "%s%s", DIR_FOR_SAVING, fileName);
+		FILE *file;
+		file = fopen(outputFile, "wb");
+		int read = 0;
+		int fullSize;
+
+		do
+		{
+			char buff[MAX_BUFFER_LENGTH];
+			int readed = recv(DATA, buff, sizeof(buff), 0);
+			fwrite(buff, 1, readed, file);
+			fullSize = read;
+			read += readed;
+		} while (fullSize != read);
+
+		readServ();
+		cout << "Сохранено в " << DIR_FOR_SAVING << endl;
+		fclose(file);
+		return 0;
+	}
 }
 
 int FTP::deleteFile(char *fileForDelete)
 {
-	char inquiry[1000];
+	char inquiry[MAX_STRING_LENGTH];
 
 	sprintf(inquiry, "DELE %s\r\n", fileForDelete);
 	sending(inquiry);
 	Sleep(SLEEP);
-	char buff[1024];
-    for (int i = 0; i<1024; i++)
+	char buff[MAX_BUFFER_LENGTH];
+	for (int i = 0; i<MAX_BUFFER_LENGTH; i++)
 		buff[i] = '\0';
-	recv(FTPsocket, buff, 1024, 0);
-	if (buff[0]=='5')
+	recv(FTPsocket, buff, MAX_BUFFER_LENGTH, 0);
+	if (buff[0] == '5')
+	{
+		cout << buff << endl;
 		return -1;
+	}
 	else
 	{
 		cout << "Server: " << buff;
@@ -216,13 +226,12 @@ int FTP::deleteFile(char *fileForDelete)
 
 void FTP::goToDirectory(char *nameOfDirectory)
 {
-	char inquiry[1000];
+	char inquiry[MAX_STRING_LENGTH];
 
 	sprintf(inquiry, "CWD %s\r\n", nameOfDirectory);
 	sending(inquiry);
 	Sleep(SLEEP);
 	readServ();
-	getList();
 }
 
 void FTP::returnToParentsDir()
@@ -230,51 +239,88 @@ void FTP::returnToParentsDir()
 	sending("XCUP\r\n");
 	Sleep(SLEEP);
 	readServ();
-	getList();
 }
 
-void FTP::close ()
+void FTP::close()
 {
 	sending("QUIT\r\n");
 	readServ();
 	system("pause");
 
-	closesocket(FTPsocket); 
+	strcpy(addressOfServer, "");
+	closesocket(FTPsocket);
+	closesocket(DATA);
 	WSACleanup();
 }
 
-void FTP:: abort ()
+void FTP::abort()
 {
 	sending("ABOR\r\n");
 	Sleep(SLEEP);
 	readServ();
-	cout << "Передача данных прервана."<< endl;
+	cout << "Передача данных прервана." << endl;
 }
 
-void FTP::putFile(char *fileName)
+int FTP::putFile(char *fileName)
 {
 	initData();
 
 	char inquiry[1000];
+
 	sprintf(inquiry, "STOR %s\r\n", fileName);
 	sending(inquiry);
 	Sleep(SLEEP);
-	readServ();
+	char buff[MAX_BUFFER_LENGTH];
+	for (int i = 0; i<MAX_BUFFER_LENGTH; i++)
+		buff[i] = '\0';
+	recv(FTPsocket, buff, MAX_BUFFER_LENGTH, 0);
+	if (buff[0] == '5')
+	{
+		cout << buff << endl;
+		return -1;
+	}
 
-	char fileN [1000];
-	sprintf(fileN, "d:/%s\r\n", fileName);
+	char fileN[1000];
+	sprintf(fileN, "%s%s",DIR_FOR_SAVING, fileName);
 
 	FILE *file;
-	file = fopen(fileN, "wb");  
-	int read = 0;
-	int fullSize;
-
-	while (file)
+	file = fopen(fileN, "rb");
+	if (file == 0)
 	{
-		char singleBuffer[2048];
-		fread(singleBuffer,1,2048,file); 
-		send(FTPsocket, singleBuffer, sizeof(singleBuffer), 0);
+		cout << "Такого файла не существует в " << DIR_FOR_SAVING  << endl;
+		return -1;
 	}
-	readServ();
+
+	fseek(file, 0, SEEK_END);
+	long size = ftell(file);
+
+	int read = 0;
+	int fullSize=0;
+	int reading;
+
+	rewind(file);
+
+	if (size > MAX_BUFFER_LENGTH)
+	{
+		do
+		{
+			char buff[MAX_BUFFER_LENGTH];
+			fread(buff, sizeof(buff), 1, file);
+			reading = send(DATA, buff, sizeof(buff), 0);
+			fullSize += reading;
+		} while (fullSize < size - MAX_BUFFER_LENGTH);
+	}
+
+	do
+	{
+		char buff[1];
+		fread(buff, sizeof(buff), 1, file);
+		reading = send(DATA, buff, sizeof(buff), 0);
+		fullSize += reading;
+	} while (fullSize!=size);
+
+	closesocket(DATA);
 	fclose(file);
-} 
+	readServ();
+	return 0;
+}
